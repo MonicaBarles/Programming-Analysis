82be6f7a5da6aeba352433aef41be58229f00230
MonicaBarles <32792658+MonicaBarles@users.noreply.github.com>
2018-02-11 19:49:28 +0800

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,71 @@
+
+#running in Windows
+
+fileURL="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL,destfile="./Dataset.zip")
+unzip(zipfile="./Dataset.zip",exdir="./Data")
+
+#PART 1_MERGE THE TRAINING TEST AND TEST TEST TO CREATE ONE DATA SET
+#read the data sets then combine
+
+#read train data sets
+trainset <- read.table("./Data/UCI HAR Dataset/train/X_train.txt")
+trainactivity <- read.table("./Data/UCI HAR Dataset/train/y_train.txt")
+trainsubject <- read.table('./Data/UCI HAR Dataset/train/subject_train.txt')
+
+#read train data sets
+testset <- read.table("./Data/UCI HAR Dataset/test/X_test.txt")
+testactivity <- read.table("./Data/UCI HAR Dataset/test/y_test.txt")
+testsubject <- read.table("./Data/UCI HAR Dataset/test/subject_test.txt")
+
+#readfeatures
+features<-read.table("./Data/UCI HAR Dataset/features.txt")
+
+#combine training and test tables
+activity<-rbind(trainactivity,testactivity)
+subject<-rbind(trainsubject,testsubject)
+set<-rbind(trainset,testset)
+
+#set names to variables
+names(activity)<-c("activity")
+names(subject)<-c("subject")
+names(set)<-features[,2]
+
+#merge all data
+mergeddata<-cbind(subject,activity,set)
+
+
+#PART 2_EXTRACTS ONLY THE MEASUREMENTS ON THE MEAN AND STANDARD DEVIATION FOR EACH MEASUREMENT
+
+#find values of mean or standard deviation
+meanstd<-grep("mean\\(\\)|std\\(\\)", features$V2,value=TRUE)
+meanstd<-union(c("subject","activity"),meanstd)
+mergeddata<-subset(mergeddata,select=meanstd)
+
+
+#PART 3_USES DESCRIPTIVE ACTIVITY NAMES TO NAME THE ACTIVITIES IN THE DATA SET
+
+#read activity file
+activitylabels<-read.table("./Data/UCI HAR Dataset/activity_labels.txt")
+activitylabels <- as.character(activitylabels[,2])
+mergeddata$activity <- activitylabels[mergeddata$activity]
+
+
+
+#PART 4_APPROPRIATELY LABELS THE DATA SET WITH DESCRIPTIVE VARIABLE NAMES
+names(mergeddata)<-gsub("std()", "standarddev", names(mergeddata))
+names(mergeddata)<-gsub("mean()", "mean", names(mergeddata))
+names(mergeddata)<-gsub("^t", "time", names(mergeddata))
+names(mergeddata)<-gsub("tBody", "TimeBody", names(mergeddata))
+names(mergeddata)<-gsub("^f", "frequency", names(mergeddata))
+names(mergeddata)<-gsub("Acc", "Accelerometer", names(mergeddata))
+names(mergeddata)<-gsub("Gyro", "Gyroscope", names(mergeddata))
+names(mergeddata)<-gsub("Mag", "Magnitude", names(mergeddata))
+names(mergeddata)<-gsub("BodyBody", "Body", names(mergeddata))
+
+#PART 5_FROM THE DATA SET IN STEP 4, CREATES A SECOND, INDEPENDENT TIDY DATA SET WITH THE AVERAGE OF EACH VARIABLE FOR EACH ACTIVITY AND EACH SUBJECT.
+
+tidydata<-aggregate(. ~subject + activity, mergeddata, mean)
+tidydata<-tidydata[order(tidydata$subject,tidydata$activity),]
+write.table(tidydata, file = "./Data/tidydata.txt",row.name=FALSE)
+save.image(file="run_analysis.RData")
